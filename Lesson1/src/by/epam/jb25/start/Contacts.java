683d4cadb2cbf6e4b8b095f13622d21c09837b5e
Volha_Dubovik <Volha_Dubovik@EPBYMINW1090.minsk.epam.com>
2018-06-11 14:51:42 +0300

    First Commit

---
+++ Lesson1/src/by/epam/jb25/start/Contacts.java
@@ -0,0 +1,11 @@
+package by.epam.jb25.start;
+
+public class Contacts {
+	public static void main (String[] args) {
+		
+		System.out.println("ФИО: Дубовик Ольга Альбиновна" + 
+		"\nАдрес: д. Новашино ул. Новаш 202" + 
+		"\nТелефон: +375 29 7761690");
+	}
+
+}
---
+++ Lesson1/src/by/epam/jb25/start/Sum4.java
@@ -0,0 +1,23 @@
+package by.epam.jb25.start;
+
+public class Sum4 {
+	public static void main (String[] args) {
+		
+		int a;
+		int b;
+		double c;
+		double d;
+		double sum;
+		
+		a = 3;
+		b = 4;
+		c = 5.5;
+		d = 6.2;
+		
+		sum = a + b + c + d;
+		
+		System.out.println("Sum = " + sum);
+		
+	}
+
+}
